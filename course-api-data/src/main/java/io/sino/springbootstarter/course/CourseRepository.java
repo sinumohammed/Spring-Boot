400b2dd72b9346b5a4c1901960dc3d2583d62824
unknown <you@example.comsinumohammed@gmail.com>
2018-03-15 15:14:10 +0530

    add to git hub

---
+++ course-api-data/src/main/java/io/sino/springbootstarter/course/CourseRepository.java
@@ -0,0 +1,11 @@
+package io.sino.springbootstarter.course;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String> {
+	
+	public List<Course> findByTopicId(String topicId);
+
+}
